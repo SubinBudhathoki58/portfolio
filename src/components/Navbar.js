2f49a2e681ebcce02d749594b4597d76f1e8cfe7
Subin Budhathoki <subinbudhathoki09@gmail.com>
2024-01-25 22:31:31 +0545

    forkupdate

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -91,7 +91,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/sandeshstaula/Portfolio"
+                href="https://github.com/SubinBudhathoki58"
                 target="_blank"
                 className="fork-btn-inner"
               >
